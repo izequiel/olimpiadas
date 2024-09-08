3fe38d4161c7bffb1451d91f1b93050fb4eec484
izequiel <izequiel@gmail.com>
2024-09-08 09:56:51 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao:"Rayssa Leal, a Fadinha, é uma skatista profissional brasileira, conhecida por seus movimentos inovadores e pela sua juventude. Ela se tornou um ícone do skate feminino no Brasil e no mundo.",
+        link:"https://pt.wikipedia.org/wiki/Raissa_Leal",
+        tags:"skate fada sk8" 
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao:"Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e determinação.",
+        link:"https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags:"judô judo judoka" 
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao:"Beatriz Souza é uma judoca brasileira que se destaca por sua técnica e força. Ela conquistou diversas medalhas em competições nacionais e internacionais, representando o Brasil com muita garra.",
+        link:"https://pt.wikipedia.org/wiki/Raissa_Leal",
+        tags:"ginastica ginástica ouro" 
+    }
+];
